8655f75d5afe7a2bb4c2b19b9d9f5b565332333e
Dev Sharma <dev.18715@knit.ac.in>
2020-11-26 20:45:49 +0530

    Improved UI

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,63 @@
+//Header Fixed
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 100) {
+    $('#header').addClass('header-scrolled');
+  } else {
+    $('#header').removeClass('header-scrolled');
+  }
+});
+
+if ($(window).scrollTop() > 100) {
+  $('#header').addClass('header-scrolled');
+}
+
+
+//Initialize VenoBox
+
+$(window).on('load', function() {
+  $('.venobox').venobox({
+    bgcolor: '',
+    overlayColor: 'rgba(6, 12, 34, 0.85)',
+    closeBackground: '',
+    closeColor: '#fff',
+    share: false
+  });
+});
+
+//Initialize SuperFish
+
+$('.navbar-nav').superfish({
+  animation: {
+    opacity: 'show'
+  },
+  speed: 400
+});
+
+
+//Navigation active state on Scroll
+
+var nav_sections = $('section');
+  var main_nav = $('.navbar-nav, #mobile-nav');
+
+  $(window).on('scroll', function() {
+    var cur_pos = $(this).scrollTop() + 200;
+
+    nav_sections.each(function() {
+      var top = $(this).offset().top,
+        bottom = top + $(this).outerHeight();
+
+      if (cur_pos >= top && cur_pos <= bottom) {
+        if (cur_pos <= bottom) {
+          main_nav.find('li').removeClass('nav-item');
+        }
+        main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('nav-item');
+      }
+      if (cur_pos < 300) {
+        $(".navbar-nav li:first").addClass('nav-item');
+      }
+    });
+  });
 //Init AOS
 
 function aos_init() {
@@ -11,5 +71,19 @@ $(window).on('load', function() {
 });
 })(jQuery);
 
-/*Automatic scroll to top when refreshing Prevention*/
+//Back to Top
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 100) {
+    $('.back-to-top').fadeIn('slow');
+  } else {
+    $('.back-to-top').fadeOut('slow');
+  }
+});
+$('.back-to-top').click(function() {
+  $('html, body').animate({
+    scrollTop: 0
+  }, 1500, 'easeInOutExpo');
+  return false;
+});
 
